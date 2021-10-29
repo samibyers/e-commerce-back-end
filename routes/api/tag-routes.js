e23b99bfe20c908de26768271b4d98bcce194a92
Sami Byers <samtaylor.byers@gmail.com>
2021-10-28 19:52:25 -0500

    routes files all filled out. everything else mostly done, but still getting errors

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,81 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    if (!tagData) {
+      res.status(404).json({message: 'There is no tag data that matches your search.'})
+      return
+    };
+  } catch (err) {
+      console.log(err)
+      res.status(500).json(err)
+  };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body)
+    res.status(200).json(newTag)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  };
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updateTag[0]) {
+      res.status(404).json({message: 'There is no tag data that matches your search.'})
+      return
+    };
+    res.status(200).json(updateTag)
+  } catch (err) {
+    res.status(500).json(err)
+  };
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({message: 'There is no tag data that matches your search.'})
+      return
+    };
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  };
 });
 
 module.exports = router;
